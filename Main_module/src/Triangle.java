78a6352a38483a24e05bb819720c1ffc0f6f8a9f
Vladimir Vozniak <vozniak.vladimir90@gmail.com>
2020-11-04 14:47:17 +0300

    Add condition of task

--- Main_module/src/Triangle.java
+++ Main_module/src/Triangle.java
@@ -1,5 +1,8 @@
 import static java.lang.Math.*;
 
+//Описать класс, представляющий треугольник.
+//Предусмотреть методы для создания объектов, вычисления площади, периметра и точки пересечения медиан.
+
 public class Triangle {
     private static double side_a;
     private static double side_b;
